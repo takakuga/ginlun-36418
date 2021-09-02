fbafec2d0025c4ebd43b2ef98404282263e50763
takakuga <sazanami398@gmail.com>
2021-09-02 17:29:25 +0900

    画像投稿・編集・削除実装

--- app/controllers/eats_controller.rb
+++ app/controllers/eats_controller.rb
@@ -1,7 +1,7 @@
 class EatsController < ApplicationController
   
   def index
-    # @eats = Eat.all
+    @eats = Eat.all
   end
 
   def new
@@ -15,11 +15,36 @@ class EatsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+  @eat = Eat.find(params[:id])
+  # @comment = Comment.new
+  # @comments = @prototype.comments.includes(:user)
+  end
+
+  def edit
+    @eat = Eat.find(params[:id])
+  end
+
+  def update
+    @eat = Eat.find(params[:id])
+    if @eat.update(eat_params)
+      redirect_to root_path
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    eat = Eat.find(params[:id])
+    eat.destroy
+     redirect_to root_path
+  end
 end
 
 private
 
   def eat_params
     params.require(:eat).permit(:title, :category, :price, :detail, :image).merge(user_id: current_user.id)
-  end
   end
--- app/models/eat.rb
+++ app/models/eat.rb
@@ -1,4 +1,12 @@
 class Eat < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :category
+    validates :price
+    validates :detail
+    validates :image
+  end
 end
