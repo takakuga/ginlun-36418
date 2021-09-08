3b684b3645bdb937fda3e69830b7f42fcf20a74e
takakuga <sazanami398@gmail.com>
2021-09-08 14:22:47 +0900

    追加レイアウト

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,11 @@ class CommentsController < ApplicationController
   end
 end
 
+  def destroy
+    Comment.find_by(id: params[:id],eat_id: params[:eat_id]).destroy
+    redirect_back(fallback_location: root_path)
+  end
+
 private
 
   def comment_params
